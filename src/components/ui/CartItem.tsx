10098140425fc84531aea318fb84d142d30c3ed6
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-16 18:43:17 -0300

    feat: ensure product quantity can be modified on cart

--- src/components/ui/CartItem.tsx
+++ src/components/ui/CartItem.tsx
@@ -1,13 +1,20 @@
-import { CartProduct } from "@/providers/cart";
+import { CartProduct, cartContext } from "@/providers/cart";
 import Image from "next/image";
 import { Button } from "./button";
 import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
 
 interface CartItemProps {
   product: CartProduct;
 }
 
 export function CartItem({ product }: CartItemProps) {
+  const { decreaseProductQuantity } = useContext(cartContext);
+
+  const handleDecreaseProductQuantityClick = () => {
+    decreaseProductQuantity(product.id);
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
@@ -35,7 +42,12 @@ export function CartItem({ product }: CartItemProps) {
           </div>
 
           <div className="flex items-center gap-1">
-            <Button size="icon" variant="outline" className="h-8 w-8">
+            <Button
+              size="icon"
+              variant="outline"
+              className="h-8 w-8"
+              onClick={handleDecreaseProductQuantityClick}
+            >
               <ArrowLeftIcon size={16} />
             </Button>
 
