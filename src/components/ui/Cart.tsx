a5ce519e66c0c48ccb7c10ea33fcef31a4a21666
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-16 18:06:02 -0300

    feat: ensure product can be added to cart

---
+++ src/components/ui/Cart.tsx
@@ -0,0 +1,23 @@
+import { ShoppingCartIcon } from "lucide-react";
+import { Badge } from "./badge";
+import { useContext } from "react";
+import { cartContext } from "@/providers/cart";
+
+export default function Cart() {
+  const { products } = useContext(cartContext);
+  return (
+    <div>
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShoppingCartIcon size={16} />
+        Catálogo
+      </Badge>
+
+      {products.map((product) => (
+        <h1 key={product.id}>{product.name}</h1>
+      ))}
+    </div>
+  );
+}
