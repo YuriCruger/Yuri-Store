c9f1c5c77a0a2e73c0218452a41f5d455297500e
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-17 19:58:10 -0300

    fix:  types in route.ts to meet Next.js expectations

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,13 @@
 import { prismaClient } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import type { NextApiHandler } from "next";
 import type { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const adapter = PrismaAdapter(prismaClient);
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: adapter as any,
   providers: [
     GoogleProvider({
@@ -16,5 +17,5 @@ export const authOptions: AuthOptions = {
   ],
 };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+const handler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
+export default handler;
